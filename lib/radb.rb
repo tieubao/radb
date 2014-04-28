a432278956767c4e7d6e756a9888570a5e42a192
Tieu Bao <nntruonghan@gmail.com>
2014-04-28 18:59:39 +0700

    add push()

--- lib/radb.rb
+++ lib/radb.rb
@@ -33,7 +33,7 @@ module Radb
             run "adb #{which_one(target)} shell \"run-as #{package} chmod 600 #{database_path}\"", :verbose => false
 
             say " > Pull images from " + images_path
-            run "adb #{which_one(target)} pull #{images_path}", :verbose => false
+            run "adb #{which_one(target)} pull #{images_path} #{options[:image_dir]} ", :verbose => false
         end
 
         #
@@ -42,13 +42,24 @@ module Radb
         `push <package name>` will push database and images from your computer to android device.
 
         Example:
-        \x5> $ radb push com.cloudjay.com -db cjay.db -i CJay/
+        \x5> $ radb push com.cloudjay.com -d cjay.db -i CJay/
         \x5>
         LONGDESC
-        option :database, :type => :string, :desc => 'Database filepath', :required => true, :aliases => "-db"
+        option :database, :type => :string, :desc => 'Database filepath', :required => true, :aliases => "-d"
         option :image_dir, :type => :string, :desc => 'Images directory', :required => true, :aliases => "-i"
+        option :serial, :type => :string, :desc => 'Device serial', :required => false, :aliases => '-s'
         def push(package)
-            say "Pushing to " + package
+
+            target = get_target(options[:serial])
+            database_dir = "/data/data/#{package}/databases/"
+            database_path = options[:database]
+            images_path = "/sdcard/DCIM/" + options[:image_dir]
+
+            say " > Push database from " + database_path + " to " + database_dir
+            run "adb #{which_one(target)} shell \"run-as #{package} chmod 666  #{database_dir}#{database_path}\"", :verbose => false
+            run "adb #{which_one(target)} push '#{database_path}' '#{database_dir}' ", :verbose => false
+            run "adb #{which_one(target)} shell \"run-as #{package} chmod 600  #{database_dir}#{database_path}\"", :verbose => false
+            run "adb #{which_one(target)} push '#{options[:image_dir]}' '#{images_path}'", :verbose => false
         end
 
         # Connect devices to PC
