bc985ad156021e07f53c383b529d842e631a5f84
Tieu Bao <nntruonghan@gmail.com>
2014-04-28 07:44:58 +0700

    add func definition

--- lib/radb.rb
+++ lib/radb.rb
@@ -1,5 +1,56 @@
 require "radb/version"
+require "thor"
+require "ADB"
 
 module Radb
-  # Your code goes here...
+
+    class Application < Thor
+        include Thor::Actions
+        include ADB
+        class_option :verbose, :type => :boolean
+
+        #
+        desc 'pull <package_name>', 'Pull database and images from android device'
+        long_desc <<-LONGDESC
+        `pull <package name>` will pull database and images from android device to your computer.
+
+        Example:
+        \x5> $ radb pull com.cloudjay.com -db cjay.db -i CJay
+        \x5>
+        LONGDESC
+        option :database, :type => :string, :desc => 'Database filename', :required => true, :aliases => "-db"
+        option :image_dir, :type => :string, :desc => 'Images directory', :required => true, :aliases => "-i"
+        def pull(package)
+            say "Pulling from " + package
+        end
+
+        #
+        desc 'push <package_name>', 'Push database and images to android device'
+        long_desc <<-LONGDESC
+        `push <package name>` will push database and images from your computer to android device.
+
+        Example:
+        \x5> $ radb push com.cloudjay.com -db cjay.db -i CJay/
+        \x5>
+        LONGDESC
+        option :database, :type => :string, :desc => 'Database filepath', :required => true, :aliases => "-db"
+        option :image_dir, :type => :string, :desc => 'Images directory', :required => true, :aliases => "-i"
+        def push(package)
+            say "Pushing to " + package
+        end
+
+        #
+        desc 'debug <ip>', 'Debug android device over the air'
+        def debug(ip)
+            # 1. Connect Android to Computer
+            # run('adb wait-for-device')
+            say "Ip: " + ip
+
+        end
+
+        #
+        desc 'logcat', 'Run logcat-color for specific device'
+        def logcat
+        end
+    end
 end
--- radb.gemspec
+++ radb.gemspec
@@ -4,20 +4,22 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'radb/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "radb"
-  spec.version       = Radb::VERSION
-  spec.authors       = ["tieubao"]
-  spec.email         = ["nntruonghan@gmail.com"]
-  spec.summary       = %q{A cli utilities which wrap Android Debug Bridge}
-  spec.description   = %q{radb provides some tool that help non-geeks take advantage on android devices}
-  spec.homepage      = "https://github.com/tieubao/radb"
-  spec.license       = "MIT"
+    spec.name          = "radb"
+    spec.version       = Radb::VERSION
+    spec.authors       = ["tieubao"]
+    spec.email         = ["nntruonghan@gmail.com"]
+    spec.summary       = %q{A cli utilities which wrap Android Debug Bridge}
+    spec.description   = %q{radb provides some tool that help non-geeks take advantage on android devices}
+    spec.homepage      = "https://github.com/tieubao/radb"
+    spec.license       = "MIT"
 
-  spec.files         = `git ls-files -z`.split("\x0")
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
-  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+    spec.files         = `git ls-files -z`.split("\x0")
+    spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+    spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+    spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.6"
-  spec.add_development_dependency "rake"
+    spec.add_development_dependency "bundler", "~> 1.6"
+    spec.add_development_dependency "rake"
+    spec.add_dependency 'thor', '>= 0.18'
+    spec.add_dependency 'ADB', '>= 0.5'
 end
